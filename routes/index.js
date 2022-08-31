6e46647eac588517d7a7a8bfa0d88b712ecc87a8
billy07001 <72130484+billy07001@users.noreply.github.com>
2022-08-31 16:36:22 +0000

    test index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//Datebase
+const Database = require("@replit/database");
+const db = new Database();
+
+db.set(1, { "name": "Billy" }).then(() => { });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.get(1).then(value => { res.send(value.name) });
 });
 
 module.exports = router;
